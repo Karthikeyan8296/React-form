749d531e57c7f445617a5ee7eb8536b36c99c817
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-09-01 19:52:23 +0530

    building form

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,12 +1,18 @@
 
-
 const Form = () => {
   return (
     <form action="">
       <div className="mb-3">
-        <label htmlFor="name" className="form-lable">Name</label>
-        <input id = "name" type="text" className="form-control" />
+        <label htmlFor="name" className="form-lable">Name uh sollu</label>
+        <input id="name" type="text" className="form-control" />
       </div>
+
+      <div className="mb-3">
+        <label htmlFor="age" className="form-lable">Age enna?</label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+
+      <button type="button" className="btn btn-primary">Submit</button>
     </form>
   );
 };
